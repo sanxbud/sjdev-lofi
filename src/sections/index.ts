8045c6cc77654f551e12874e7448046288046ba1
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-06 20:29:19 -0500

    Adding export index

---
+++ src/sections/index.ts
@@ -0,0 +1,8 @@
+
+export { Welcome } from './Welcome'
+export { Thoughts } from './Thoughts'
+export { Interests } from './Interests'
+export { Toolbelt } from './Toolbelt'
+export { Experience } from './Experience'
+export { Projects } from './Projects'
+export { Contact } from './Contact'
