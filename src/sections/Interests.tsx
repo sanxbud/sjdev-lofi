50c97b75e62f3654d19fee03a777e56c4f1498e7
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-06 20:28:21 -0500

    Created Interests Section

---
+++ src/sections/Interests.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Section } from "../components/Section";
+
+
+export const Interests = () =>{
+
+    const interestHeader = `drive --list`
+    const interestBody = `My goal is to make low-friction tools and apps that make people’s lives easier, help create the frameworks and systems that can get people closer to their goals, become the best version of myself - and do as much as I can to help build a more empathetic and human-centred world. 
+
+Everyone's on their own journey, and we all do a little better when we help each other out :)`
+
+    const interestBodyList = `What drives me is the desire to:
+
+- make low-friction tools and apps that make people’s lives easier 
+- create the frameworks and systems that help the humans using them achieve their goals
+- become the best version of myself 
+- and do as much as I can to help build a more empathetic and human-centred world. 
+
+Everyone's on their own journey, and we all do a little better when we help each other out :)`
+
+
+    return(
+        <Section header={interestHeader} body={interestBody} />
+    )
+}
+
+
+
