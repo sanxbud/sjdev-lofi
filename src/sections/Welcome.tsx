6aa16add9eccd848d52a283a8d5dc1e92c9c655a
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-06 19:30:21 -0500

    Created Welcome section

---
+++ src/sections/Welcome.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Section } from "../components/Section";
+                
+
+export const Welcome = () =>{
+
+    const welcomeHeader = `welcome.sh`
+    const welcomeBody = `I'm a dev from Winnipeg with a background in computer science and history. I care about kindness, honesty, always improving, and always finding joy in whatever I'm doing
+
+My goal is to make low-friction tools and apps that make people’s lives easier, help create the frameworks and systems that can get people closer to their goals, and do as much as I can to help build a more empathetic and human-centred world. Everyone's on their own journey, and we all do a little better when we help each other out :)`
+    
+    
+
+    return(
+        <Section></Section>
+
+
+    )
+}
