c09211515c2e7163c841f94c0167a6fd8ef315e6
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-09 19:49:25 -0500

    Updating section text

--- src/sections/Whois.tsx
+++ src/sections/Whois.tsx
@@ -5,7 +5,7 @@ import { Section } from "../components/Section";
 export const Whois = () =>{
 
     const welcomeHeader = `whois Sanjay`
-    const welcomeBody = `Hey, I’m Sanjay — a developer from Winnipeg with a background in computer science and history. I care about kindness, honesty, always improving, and always finding joy in whatever I’m doing :)
+    const welcomeBody = `Hey, I’m a developer from Winnipeg with a background in computer science and history. I care about kindness, honesty, always improving, and always finding joy in whatever I’m doing :)
 
 
 Right now, I'm focused on writing (blog coming soon!), doing some research on AI, and upping my Go game with a couple apps and tools I have in development. 
