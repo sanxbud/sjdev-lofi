ce6e510ab5b576c6147414e81388c3271c8ec163
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-09 20:02:32 -0500

    Adding resume links

--- src/sections/Interests.tsx
+++ src/sections/Interests.tsx
@@ -5,7 +5,7 @@ import { Section } from "../components/Section";
 export const Interests = () =>{
 
     const interestHeader = `grep -i "interests" brain.dmp`
-    const interestBody = `My goal is to build low-friction tools and apps that make people’s lives easier, help create the frameworks and systems that can get people closer to their goals, become the best version of myself - and do as much as I can to help build a more empathetic and human-centred world. 
+    const interestBody = `My goal is to build low-friction tools and apps that make people’s lives easier, help create the frameworks and systems that can get people closer to their goals, become the best version of myself and do as much as I can to help build a more empathetic and human-centred world along the way! 
 \n\n
 Everyone's on their own journey, and we all do a little better when we help each other out :)`
 
