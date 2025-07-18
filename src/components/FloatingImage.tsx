6aca341ccf8293757bcd195eaca9801bbcce327c
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-17 23:47:16 -0500

    Aligning images off centre rather than left

--- src/components/FloatingImage.tsx
+++ src/components/FloatingImage.tsx
@@ -14,7 +14,7 @@ export const FloatingImage = ({ src,x,y,rotate,width }:ImageProps) =>{
         <div className='absolute max-w-none fade bg-white p-[1rem] pb-[2.5rem] shadow-[0_0.5rem_1rem_0_rgba(0,0,0,0.5)] rounded-sm' 
             style={{ 
                 top: `${y}rem`, 
-                left: `${x}rem`, 
+                left: `calc(50% - ${width / 2}rem + ${x}rem)`, 
                 transform: `rotate(${rotate}deg)`,
                 width: `${width}rem`
             }}>
