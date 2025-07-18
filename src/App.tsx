8224033ac37568004a9f86ebadec2b36c3d23560
rook37 <33361387+rook37@users.noreply.github.com>
2025-07-17 23:47:47 -0500

    Including images in background, adding fade animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import lilguy from './assets/ugotthislilguy.png'
+import meNZ from './assets/images/me_nz.jpg'
+import cow from './assets/images/cow.jpg'
+import caniplayit from './assets/images/caniplayit.png'
 import { Welcome, Interests, Toolbelt, Experience, Projects, Contact, Whois } from './sections'
+import { FloatingImage } from './components/FloatingImage'
 
 function App() {
   const [visibleIndex, setVisibleIndex] = useState(0)
@@ -26,11 +30,20 @@ function App() {
     <Contact key="contact" />,
   ]
 
-  return (<>
-        <img src={lilguy} alt="you got this" className="mx-auto w-40"/>
-    <div className="w-screen  max-w-6xl mx-auto px-10 py-14 pt-px text-left items-start text-lg space-y-10 font-mono whitespace-pre-wrap">
-      {sections.slice(0, visibleIndex + 1)}
-    </div></>
+  return (
+
+        <div className="overflow-x-hidden">
+        
+ <FloatingImage src={meNZ} x={22} y={18} rotate={-3} width={55} />
+            <FloatingImage src={cow} x={-22} y={65} rotate={3} width={55}/>
+            <FloatingImage src={caniplayit} x={15} y={115} rotate={-4} width={65}/>
+
+            <div className="relative w-screen max-w-6xl mx-auto px-10 py-14 pt-px text-left items-start text-lg space-y-10 font-mono whitespace-pre-wrap" style={{zIndex:2}}>
+            
+                <img src={lilguy} alt="you got this" className="mx-auto w-40"/>
+                {sections.slice(0, visibleIndex + 1)}
+            </div>
+        </div>
   )
 }
 
